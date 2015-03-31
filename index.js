486bc8b3b7789cfeef7026995eea0fa0efc25504
JD Isaacks <jd@jisaacks.com>
2015-03-31 11:55:35 -0400

    Prepare for heroku

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ var tasksRouter = require("./tasks_router");
 
 app.use("/tasks", tasksRouter);
 
-
-console.log("Listening on :8025");
-app.listen(8025);
+var port = process.env.PORT || 8025;
+console.log("Listening on :"+port);
+app.listen(port);
